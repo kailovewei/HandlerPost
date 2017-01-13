7cbd0a713cdfab0d3acc6a347f6e20baa2821078
kailovewei <1045110506@qq.com>
2017-01-13 15:32:04 +0800

    description is detailed

--- app/src/main/java/ykk/handlerpost/com/handlerpost/MainActivity.java
+++ app/src/main/java/ykk/handlerpost/com/handlerpost/MainActivity.java
@@ -21,11 +21,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+/*
+private static Message getPostMessage(Runnable r)
+1.该方法完成了两个操作，第一生成了一个Message对象。第二,将r对象赋值给Message对象的callback属性
+ 第一个问题：如何把一个Runnable对象放置在一个消息队列当中；实际是生成一个Message对象，
+ 并将r赋值给Message的callback属性，然后将Message放置在消息队列当中。
+ 第二个问题：Looper取出了携带r对象的Message对象之后，调用了dispatchMessage()方法，然后判断Message的callback属性是否为空，
+ 此时callback属性有值，所以执行了handleCallback(Message msg),在方法中执行了msg.callback.run();
+
+ */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button button;
     private EditText editText;
     private Handler handler;
-    private String line;
+    private static String line;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +46,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         handler=new Handler();
 
     }
-
     @Override
     public void onClick(View v) {
         WorkThread thread=new WorkThread();
@@ -48,6 +56,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     {
         @Override
         public void run() {
+            //从网络中取回数据
             HttpClient httpClient=new DefaultHttpClient();
             HttpGet httpGet=new HttpGet("http://www.marschen.com/data1.html");
             try {
@@ -65,9 +74,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Runnable r=new Runnable() {
 
                 public void run() {
+                    //在这里更新UI代码
                             editText.setText(line);
                     }
             };
+            //post(r)方法将应用r对象放置在消息队列当中，Looper对象（主线程）从消息队列当中取出了r对象。
             handler.post(r);
         }
     }
